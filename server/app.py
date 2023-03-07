9e52ac81c8fe9d1cfee1896ded177dc10d89c8a1
FaithCyril-star <80582712+FaithCyril-star@users.noreply.github.com>
2023-03-07 12:17:52 +0000

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 import os
 import openai
 from flask import Flask,request
+import nltk
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -8,22 +9,40 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class Chat:
     def __init__(self):
         self.conversation = []
+        self.token_size = 0
+
+    def add_message(self,message):
+        message_text = message['content']
+        tokens = nltk.word_tokenize(message_text)
+        self.token_size += len(tokens)
+
+        self.conversation.append(message)
+
+    def clear_chat(self):
+        self.conversation = []
+        self.token_size = 0
+
 
 chat = Chat()
 
 @app.route("/", methods=["GET","POST"])
 def conversation():
     if request.method == "GET":
-        chat.conversation = [{"role": "system", "content": "You are a therapist and friend. Your name in every conversation is Lana always, do not forget. Respond briefly"}]
+        chat.clear_chat()
+        chat.add_message({"role": "system", "content": "You are a therapist and friend. Your pretend name in every conversation is Lana always, do not forget. Introduce yourself as Lana and respond briefly"})
         completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=chat.conversation)
-        
+
         response = "New conversation initiated"
     else:
-        message = request.json['info']
-        chat.conversation.append({"role":"user","content":message})
-       
+        if chat.token_size > 3075:
+            chat.clear_chat()
+            return {"response":"chat limit is reached, conversation re-initiated")
+
+        message_text = request.json['info']
+        chat.add_message({"role":"user","content":message_text})
+
 
         completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -31,8 +50,10 @@ def conversation():
         )
 
         response = completion.choices[0].message.content
-        chat.conversation.append({"role":"assistant","content":response})
+        chat.add_message({"role":"assistant","content":response})
 
     return {"response": response}
+
+
 
     
