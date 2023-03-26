1549c2b0df7c5c9ff2fed415331e18da0840c307
FaithCyril-star <80582712+FaithCyril-star@users.noreply.github.com>
2023-03-26 22:36:16 +0000

    Update app.py

--- server/app.py
+++ server/app.py
@@ -2,27 +2,12 @@ import os
 import openai
 from flask import Flask,request
 import nltk
-
+from myChat import Chat
 nltk.download('punkt')
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-class Chat:
-    def __init__(self):
-        self.conversation = []
-        self.token_size = 0
-
-    def add_message(self,message):
-        message_text = message['content']
-        tokens = nltk.word_tokenize(message_text)
-        self.token_size += len(tokens)
-
-        self.conversation.append(message)
-
-    def clear_chat(self):
-        self.conversation = [{"role": "system", "content": "You are a therapist and friend. Your pretend name in every conversation is Lana always, do not forget. Introduce yourself as Lana and respond briefly"}]
-        self.token_size = 0
 
 
 chat = Chat()
